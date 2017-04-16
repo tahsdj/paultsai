cb551c0f3318f3d4919923a8cd0f9b7a84d76cc1
tahsdj <tahsdj@gmail.com>
2017-04-17 00:35:24 +0800

    finish

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
-$(function(){
+$(() =>{
 	let scroll_position = 0
 	$(window).scroll(function(){
 	    let scrollVal = $(this).scrollTop()
 	    let speed = 50
 	    if ( scrollVal >= 75 ) {
-	    	$('header').css({
+	    	$('.top').css({
 	    		'height':'80px',
 	    		//'background-color':'white'
 	    	})
 	    }else{
-	    	$('header').css({
+	    	$('.top').css({
 	    		'height':'280px',
 	    		'opacity':'1',
 	    		'background-color':'#A5DEF1'
 	    	})
 	    }
+	})
+});
+
+$(() =>{
+	$('.projects > header > a').click(function (){
+		let index = $(this).index()
+		const $current = $('.projects .container > div').eq(index)
+		$current.children('.arrow-up').css({
+			'left':'calc((100%/3)/2 - 10px + '+index+'*(100%/3))'
+		})
+		$current.addClass('current').siblings('.current').removeClass('current')
 	})
 })
